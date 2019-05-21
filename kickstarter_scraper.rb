57fc48dbfed779c7477aea472d54d573730e6b3b
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-05-21 02:59:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,13 @@ require 'nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+# Iterate through the projects
+kickstarter.css("li.project.grid_4").each do |project|
+  projects[project] = {}
+end
   binding.pry
 end
 
